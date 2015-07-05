a1ade6a533b8ab26bf83610a6e90cbdf63027833
testOfCoder <1531990876@qq.com>
2015-07-05 14:32:43 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by wmg on 15/7/5.
+//  Copyright (c) 2015年 test1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by wmg on 15/7/5.
+//  Copyright (c) 2015年 test1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
